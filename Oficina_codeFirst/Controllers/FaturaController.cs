3b22df474359c0580f608e35791a1e4461542233
Petermon <petersonvgama@gmail.com>
2018-06-11 11:21:34 -0300

    Adicionando controllers e views

---
+++ Oficina_codeFirst/Controllers/FaturaController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Oficina_codeFirst.Models;
+
+namespace Oficina_codeFirst.Controllers
+{
+    public class FaturaController : Controller
+    {
+        private OficinaContext db = new OficinaContext();
+
+        // GET: /Fatura/
+        public ActionResult Index()
+        {
+            var fatura = db.Fatura.Include(f => f.Atendimento);
+            return View(fatura.ToList());
+        }
+
+        // GET: /Fatura/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fatura fatura = db.Fatura.Find(id);
+            if (fatura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fatura);
+        }
+
+        // GET: /Fatura/Create
+        public ActionResult Create()
+        {
+            ViewBag.Oid = new SelectList(db.Atendimentos, "Oid", "Codigo");
+            return View();
+        }
+
+        // POST: /Fatura/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Oid,ValorRecebido,TipoPagamento,parcelas")] Fatura fatura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Fatura.Add(fatura);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Oid = new SelectList(db.Atendimentos, "Oid", "Codigo", fatura.Oid);
+            return View(fatura);
+        }
+
+        // GET: /Fatura/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fatura fatura = db.Fatura.Find(id);
+            if (fatura == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Oid = new SelectList(db.Atendimentos, "Oid", "Codigo", fatura.Oid);
+            return View(fatura);
+        }
+
+        // POST: /Fatura/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Oid,ValorRecebido,TipoPagamento,parcelas")] Fatura fatura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fatura).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Oid = new SelectList(db.Atendimentos, "Oid", "Codigo", fatura.Oid);
+            return View(fatura);
+        }
+
+        // GET: /Fatura/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fatura fatura = db.Fatura.Find(id);
+            if (fatura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fatura);
+        }
+
+        // POST: /Fatura/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Fatura fatura = db.Fatura.Find(id);
+            db.Fatura.Remove(fatura);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Oficina_codeFirst/Controllers/PecaController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Oficina_codeFirst.Models;
+
+namespace Oficina_codeFirst.Controllers
+{
+    public class PecaController : Controller
+    {
+        private OficinaContext db = new OficinaContext();
+
+        // GET: /Peca/
+        public ActionResult Index()
+        {
+            var peca = db.Peca.Include(p => p.Atendimento).Include(p => p.TipoPea);
+            return View(peca.ToList());
+        }
+
+        // GET: /Peca/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Peca peca = db.Peca.Find(id);
+            if (peca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peca);
+        }
+
+        // GET: /Peca/Create
+        public ActionResult Create()
+        {
+            ViewBag.AtendimentoOid = new SelectList(db.Atendimentos, "Oid", "Codigo");
+            ViewBag.TipoPecaOid = new SelectList(db.TipoPeca, "Oid", "Nome");
+            return View();
+        }
+
+        // POST: /Peca/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Oid,Codigo,AtendimentoOid,TipoPecaOid")] Peca peca)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Peca.Add(peca);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AtendimentoOid = new SelectList(db.Atendimentos, "Oid", "Codigo", peca.AtendimentoOid);
+            ViewBag.TipoPecaOid = new SelectList(db.TipoPeca, "Oid", "Nome", peca.TipoPecaOid);
+            return View(peca);
+        }
+
+        // GET: /Peca/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Peca peca = db.Peca.Find(id);
+            if (peca == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AtendimentoOid = new SelectList(db.Atendimentos, "Oid", "Codigo", peca.AtendimentoOid);
+            ViewBag.TipoPecaOid = new SelectList(db.TipoPeca, "Oid", "Nome", peca.TipoPecaOid);
+            return View(peca);
+        }
+
+        // POST: /Peca/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Oid,Codigo,AtendimentoOid,TipoPecaOid")] Peca peca)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(peca).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AtendimentoOid = new SelectList(db.Atendimentos, "Oid", "Codigo", peca.AtendimentoOid);
+            ViewBag.TipoPecaOid = new SelectList(db.TipoPeca, "Oid", "Nome", peca.TipoPecaOid);
+            return View(peca);
+        }
+
+        // GET: /Peca/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Peca peca = db.Peca.Find(id);
+            if (peca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(peca);
+        }
+
+        // POST: /Peca/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Peca peca = db.Peca.Find(id);
+            db.Peca.Remove(peca);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
