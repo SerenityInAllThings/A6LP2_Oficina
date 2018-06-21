c90cd3452448992144bf59e00534b1788dc7a1ad
Petermon <petersonvgama@gmail.com>
2018-06-21 04:18:21 -0300

    Implementação de login

---
+++ Oficina_codeFirst/Scripts/Autenticacao.js
@@ -0,0 +1,39 @@
+﻿const apiUrl = "http://localhost:1916/";
+
+$(document).ready(() => {
+    $('#status').hide();
+    $("#botao-entrar").click(() => {
+        $.ajax({
+            url: apiUrl + "Autenticacao/AutenticarUsuario",
+            dataType: 'json',
+            type: "POST",
+            async: true,
+            beforeSend: () => {
+                $('#status').html('Autenticando usuário');
+                $('#status').show();
+            },
+            data: {
+                login: $("input[name$=txtLogin]").val(),
+                senha: $("input[name$=txtSenha]").val(),
+            },
+            success: (retorno) => {
+                console.log(retorno)
+                console.log('oi')
+                if (retorno.OK) {
+                    console.log('oi2')
+                    $('#status').html(retorno.Mensagem);
+                    setTimeout(window.location.href = "/Home", 2000);
+                    $('#status').show();
+                } else {
+                    console.log('oi3')
+                    $('#status').html(retorno.Mensagem);
+                    $('#status').show();
+                }
+            },
+            error: (retorno) => {
+                $('#status').html(retorno.Mensagem);
+                $('#status').show();
+            },
+        });
+    });
+});
